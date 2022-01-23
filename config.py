fe4d2e29e95662918856ef94ced42cbb65c5a3a9
Angus Wu <wuangusjh@gmail.com>
2022-01-22 23:12:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-INFURA_URL = ""
-BSC_URL = ""
-ADDRESS = ""
+INFURA_URL = ""  # ETH API provider for web3.py
+BSC_URL = ""  # BSC API provider
+ADDRESS = ""  # ETH Address to make ledger of
 ES_TOKEN = ""  # Etherscan API Token
